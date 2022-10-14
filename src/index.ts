70f1308c2955c2b25626cf87b960759743fd930c
Johnathon <johnathondouglass@gmail.com>
2022-10-13 22:52:00 -0700

    fix: change scrapers to run 2 minutes apart from each other

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,27 @@ import { fileURLToPath } from 'node:url';
 import * as path from 'node:path';
 
 const bree = new Bree({
-  root: path.join(
-    path.dirname(fileURLToPath(import.meta.url)),
-    'abstractFactory'
-  ),
+  root: path.join(path.dirname(fileURLToPath(import.meta.url)), 'clients'),
   jobs: [
     {
-      name: 'client',
-      cron: '0 * * * *',
+      name: 'monogramClient',
+      cron: '*/48 * * * *',
+    },
+    {
+      name: 'piratesClient',
+      cron: '*/50 * * * *',
+    },
+    {
+      name: 'revolverClient',
+      cron: '*/52 * * * *',
+    },
+    {
+      name: 'rogueWaveClient',
+      cron: '*/54 * * * *',
+    },
+    {
+      name: 'rossoClient',
+      cron: '*/56 * * * *',
     },
   ],
 });
