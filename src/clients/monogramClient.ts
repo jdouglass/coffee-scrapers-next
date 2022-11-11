adcc7c8ce201fa8fad36b4094bc7ffb991075f01
Johnathon <johnathondouglass@gmail.com>
2022-11-10 19:08:49 -0800

    remove console log

--- src/clients/monogramClient.ts
+++ src/clients/monogramClient.ts
@@ -73,7 +73,6 @@ export class MonogramClient {
           weight,
           vendor: this.vendor,
         };
-        console.log(product);
         this.monogramProducts.push(product);
         await browser.close();
       }
